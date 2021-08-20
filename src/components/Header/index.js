5c878db116fca1cc61343c8209097d1dd60044e9
Ehtisham <ehtishamhassan9@gmail.com>
2021-08-20 23:01:18 +0500

    partially refactored product details page with tailwind css

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,22 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export default function Header() {
     return (
         <div className="bg-white border mb-8">
             <div className="container mx-auto py-4">
-                <h2 className={"text-3xl font-semibold text-gray-700"}>
+                <Link to={"/"}>
+                    <h2 className={"text-3xl font-semibold text-gray-700"}>
                     <span className={"text-blue-500"}>
                         React
                     </span>
-                    -
-                    <span className={"text-purple-700"}>
+                        -
+                        <span className={"text-purple-700"}>
                         Redux &nbsp;
                     </span>
-                    Shop
-                </h2>
+                        Shop
+                    </h2>
+                </Link>
             </div>
         </div>
     );
--- src/components/ProductDetails/index.js
+++ src/components/ProductDetails/index.js
@@ -2,36 +2,35 @@ import React from 'react'
 import {useSelector} from "react-redux";
 import {useParams} from "react-router-dom"
 
+
 function ProductDetails() {
 
     const {productId} = useParams()
-
     console.log(useParams())
 
     const product = useSelector(state => state.products.find(product => product.id == productId))
     const {image, title, price, category, description} = product || {}
 
     return (
-        <div className="ui placeholder segment">
-            <div className="ui two column stackable center aligned grid">
-                <div className="ui vertical divider">AND</div>
-                <div className="middle aligned row">
-                    <div className="column lp">
-                        <img className="ui fluid image" src={image}/>
+        <div className="">
+            <div className="flex">
+                <div className="md:w-1/2 p-4">
+                    <div className={"p-4 border h-80 bg-white"}>
+                        <img src={image} className={"w-full h-full object-contain"}/>
                     </div>
-                    <div className="column rp">
-                        <h1>{title}</h1>
-                        <h2>
-                            <a className="ui teal tag label">${price}</a>
-                        </h2>
-                        <h3 className="ui brown block header">{category}</h3>
-                        <p>{description}</p>
-                        <div className="ui vertical animated button" tabIndex="0">
-                            <div className="hidden content">
-                                <i className="shop icon"></i>
-                            </div>
-                            <div className="visible content">Add to Cart</div>
+                </div>
+                <div className="md:w-1/2 p-4">
+                    <h1 className={"text-gray-700 font-semibold text-lg"}>{title}</h1>
+                    <h2 className={"mt-4"}>
+                        <a className="ui teal tag label">${price}</a>
+                    </h2>
+                    <h3 className="ui brown block header">{category}</h3>
+                    <p className={"mb-4"}>{description}</p>
+                    <div className="ui vertical animated button " tabIndex="0">
+                        <div className="hidden content">
+                            <i className="shop icon"></i>
                         </div>
+                        <div className="visible content">Add to Cart</div>
                     </div>
                 </div>
             </div>
