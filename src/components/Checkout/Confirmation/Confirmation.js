0a7eea78d5b95ab8b44752542d0449bb6b84972d
Ehtisham <ehtishamhassan9@gmail.com>
2021-08-30 22:17:33 +0500

    Install and setup tailwindcss via npm

---
+++ src/components/Checkout/Confirmation/Confirmation.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Confirmation = () => {
+    return (
+        <div>
+            Order Confirmed. See your email.
+        </div>
+    )
+}
+
+export default Confirmation
